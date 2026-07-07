7ddf491f342e99eab6ea883f539f149c767799b7
Nguyễn Văn Toại <toainguyenprogramminggame@gmail.com>
2026-07-07 22:39:27 +0700

    feat: implement graceful shutdown

--- services/auth/cmd/main.go
+++ services/auth/cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -45,8 +50,38 @@ func main() {
 		port = "8081"
 	}
 
-	log.Printf("Auth Service is running on port %s", port)
-	if err := http.ListenAndServe(":"+port, app.Engine); err != nil {
-		log.Fatalf("Failed to run HTTP server: %v", err)
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: app.Engine,
 	}
+
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		log.Printf("Auth Service is running on port %s", port)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Failed to run HTTP server: %v", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be caught, so no need to add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatalf("Server forced to shutdown: %v", err)
+	}
+
+	log.Println("Server exiting")
 }
